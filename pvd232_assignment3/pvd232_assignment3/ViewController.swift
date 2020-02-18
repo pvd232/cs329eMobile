e34efe377e2af621bc10993248cb3333d16082b8
Driscoll <pvd232@lib-pclem010.lib.utexas.edu>
2020-02-17 18:31:34 -0600

    none

--- pvd232_assignment3/pvd232_assignment3/ViewController.swift
+++ pvd232_assignment3/pvd232_assignment3/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // Mark Properties
+    @IBOutlet weak var nameHometownLabel: UILabel!
+    @IBOutlet weak var nameLabel: UIStackView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var austinImage: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+        cityTextField.delegate = self
+        nameTextField.returnKeyType = UIReturnKeyType.done
+        cityTextField.returnKeyType = UIReturnKeyType.done
+        
+        
+    }
+    //Mark: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
     }
     
-    	
-    @IBOutlet weak var hometownLabel: UILabel!
-    func align () {
-        hometownLabel.textAlignment = .center
-    }
-
+    
 }
 
