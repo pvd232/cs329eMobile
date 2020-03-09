d49eda6f46cac23e7e24f276042e0b1b1ef44d52
Driscoll <pvd232@lib-pclem052.austin.utexas.edu>
2020-03-08 19:19:47 -0500

    everything is fucked up

--- group6_assignment5/AnimalCollectionViewController.swift
+++ group6_assignment5/AnimalCollectionViewController.swift
@@ -29,7 +29,7 @@ class GalleryItem {
     }
 }
 
-class AnimalCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class AnimalCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     
     @IBOutlet weak var AnimalCollectionView: UICollectionView!
     
@@ -37,6 +37,8 @@ class AnimalCollectionViewController: UIViewController, UICollectionViewDataSour
     
     /* String identifier used to dynamically load cells into collection */
     let identifier = "AnimalCollectionCell"
+    
+     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 
     override func viewDidLoad() {
         accessPhotoPlist()
@@ -60,7 +62,7 @@ class AnimalCollectionViewController: UIViewController, UICollectionViewDataSour
         
     // MARK: UICollectionViewDataSource
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        1
+       return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -80,13 +82,47 @@ class AnimalCollectionViewController: UIViewController, UICollectionViewDataSour
 
     //MARK: UICollectionViewFlowLayout
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return CGSize(width: 100, height: 100)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.init(top: 20, left: 10, bottom: 20, right: 10)
-    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        return CGSize(width: 100, height: 100)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets.init(top: 20, left: 10, bottom: 20, right: 10)
+//    }
 
 }
+// MARK: - UICollection View Flow Layout Delegate
+
+
+
+extension AnimalCollectionViewController : UICollectionViewDelegateFlowLayout {
+  //1
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //2
+    let itemsPerRow: CGFloat = 1
+    let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+    let availableWidth = view.frame.width - paddingSpace
+    let widthPerItem = availableWidth / itemsPerRow
+    
+    return CGSize(width: widthPerItem, height: widthPerItem)
+  }
+  
+  //3
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return sectionInsets
+  }
+  
+  // 4
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return sectionInsets.left
+  }
+}
+
+
