651cebddea72532b97a70db67019f4935636b4a5
pvd232 <patardriscoll@gmail.com>
2020-04-04 18:08:01 -0500

    git the main adventurer tableview to work and the modal segue to the create new adventurer view mostly done and finally added an empty view for the quest page with a funcional segue

---
+++ assignment6/AdventurerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AdventurerTableViewCell.swift
+//  assignment6
+//
+//  Created by Peter Vail Driscoll II on 4/4/20.
+//  Copyright © 2020 group6. All rights reserved.
+//
+
+import UIKit
+
+class AdventurerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ assignment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment6
+//
+//  Created by Peter Vail Driscoll II on 4/4/20.
+//  Copyright © 2020 group6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
